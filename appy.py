ca5f329bc2368d42442a045d7f82afe98402c18f
Gabriel Costa <ntconsult.gabriellva@agi.com.br>
2025-12-27 19:24:21 -0300

    importando classes e listando avaliacoes

---
+++ appy.py
@@ -0,0 +1,12 @@
+from modelos.restaurante import Restaurante
+
+restaurante_praca = Restaurante('praça', 'Gourmet')
+restaurante_praca.receber_avaliacao('Lucas', 6)
+restaurante_praca.receber_avaliacao('Adelvan', 5)
+restaurante_praca.receber_avaliacao('Michel', 10)
+
+def main():
+    Restaurante.listar_restaurantes()
+
+if __name__ == '__main__':
+    main()
